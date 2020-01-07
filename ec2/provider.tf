d551fd62e4a4749a092d44a2c53df2ac8576de1d
gurunikhitha <56013371+gurunikhitha@users.noreply.github.com>
2020-01-07 10:00:45 +0530

    commit

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "your_access_key"
-  secret_key = "your_secret_key"
-  region     = "eu-west-2"
+  access_key = "AKIAI5IHW4CZZNOCNFFQ"
+  secret_key = "XvMdg0sKIJ4ZcRVV+bT5/nDbix2dkAoJl2kVTanI"
+  region     = "us-east-1"
 }
